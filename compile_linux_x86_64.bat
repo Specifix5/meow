2cd75827bab510d6958943d9bd2ec8d7994980e3
Specifix5 <62927726+Specifix5@users.noreply.github.com>
2025-04-08 20:53:14 +0800

    AAAAAA

--- compile_linux_x86_64.bat
+++ compile_linux_x86_64.bat
@@ -2,7 +2,4 @@
 for /f %%i in ('git rev-parse --short HEAD') do set GIT_HASH=%%i
 for /f %%i in ('git rev-parse HEAD') do set GIT_HASH_LONG=%%i
 
-echo GIT_HASH: %GIT_HASH%
-echo GIT_HASH_LONG: %GIT_HASH_LONG%
-
-cross build --target x86_64-unknown-linux-musl --release
+start cmd /k "echo GIT_HASH: %GIT_HASH% & echo GIT_HASH_LONG: %GIT_HASH_LONG% & cross build --target x86_64-unknown-linux-musl --release"
